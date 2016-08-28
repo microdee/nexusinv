7b02a80cb19b46decdde3ac30ba79bbcbe7496d6
David Morasz <m@mcro.de>
2016-08-29 01:20:22 +0200

    Added dynamic color styling

---
+++ nodes/modules/colormod.template.js
@@ -0,0 +1,10 @@
+function colormod$nxWName$() {
+  $nxWName$.colors.accent = "$accent$";
+  $nxWName$.colors.fill = "$fill$";
+  $nxWName$.colors.border = "$border$";
+  $nxWName$.colors.black = "$black$";
+  $nxWName$.colors.white = "$white$";
+  $nxWName$.label = "$label$";
+  $nxWName$.draw();
+}
+colormod$nxWName$();
