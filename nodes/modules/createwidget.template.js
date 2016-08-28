0f59647b74c6e3b5ef23666c3b8b86ef573154cb
David Morasz <m@mcro.de>
2016-08-29 01:17:44 +0200

    Added z-index to widgets

--- nodes/modules/createwidget.template.js
+++ nodes/modules/createwidget.template.js
@@ -9,12 +9,14 @@ function create$nxWName$() {
   $("#d$nxWName$").css({
     position: "fixed",
     $vAnch$: $nxWTop$,
-    $hAnch$: $nxWLeft$
+    $hAnch$: $nxWLeft$,
+    zIndex: $nxWZ$
   });
   $("canvas:not(.nx)").remove();
   //customscript//
   $nxWName$.sendsTo(function(data) {
     vvvv.v$nxWName$("$nxWName$: " + JSON.stringify(data));
   });
+  vvvv.v$nxWName$("$nxWName$: {none: 0}");
 }
 create$nxWName$();
